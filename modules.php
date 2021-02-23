4898f8c640d6d4da478246b61551bbeeeda4183e
Ludovic Taffin <Drumor@users.noreply.github.com>
2021-02-23 11:28:05 +0100

    Update modules.php

--- modules.php
+++ modules.php
@@ -2,10 +2,7 @@
 define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/includes/common.inc';
-require_once DRUPAL_ROOT . '/includes/file.inc';
-require_once DRUPAL_ROOT . '/includes/module.inc';
-require_once DRUPAL_ROOT . '/includes/ajax.inc';
-require_once DRUPAL_ROOT . '/includes/install.inc';
+require_once DRUPAL_ROOT . '/modules/update/update.module';
 
 // We prepare only a minimal bootstrap. This includes the database and
 // variables, however, so we have access to the class autoloader registry.
@@ -14,26 +11,17 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_SESSION);
 // This must go after drupal_bootstrap(), which unsets globals!
 global $conf;
 
+// Array of data about available releases, keyed by project shortname.
+$available = update_get_available();
+// An array of installed projects with current update status information.
+$projects_with_update_status = update_calculate_project_data($available);
 
-$files = drupal_system_listing('/\.module$/', 'modules', 'name', 0);
-
-//system_get_files_database($files, 'module');
-
-ksort($files);
-
-$core_installed = array();
-
-foreach ($files as $info) {
-  $module = new stdClass();
-  $module->name = $info->name;
-  $module->version = "".drupal_get_installed_schema_version($module->name);
-  $core_installed[] = $module;
-  //echo $core_installed;
+foreach($projects_with_update_status as $module_name => $value) {
+    if($value["existing_version"] !==  $value["latest_version"]){
+        $result[] = array("name" => $module_name, "installed_version" => $value["existing_version"], "latest_version" => $value["latest_version"]);
+    }
+    
 }
-
-//print "Installed Core Modules: " . join(", ", $core_installed) . "\n\n";
-
-$myJSON = json_encode($core_installed);
-
-echo $myJSON;
+header('Content-Type: application/json');
+echo json_encode($result);
 ?>
